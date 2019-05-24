1ed0c26ada9313b1ef9e7b7141273c6d8e898b33
nhgiang196 <nhgiang196>
2019-05-24 08:48:07 +0700

    add new class

---
+++ THS.Models/MyModelAndClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHS.Models
+{
+    public class MyModelAndClass
+    {
+        public MyModelAndClass() { }
+        public string username { get; set; }
+        public string pass { get; set; }
+        public string newpass { get; set; }
+        public MyModelAndClass(string Username, string Pass, string Newpass)
+        {
+            username = Username; pass = Pass; newpass = Newpass;
+        }
+
+
+    }
+}
